1a56494e8b113de5eb4daffa898b34c1778c6d20
roaa <mroaa023@gmail.com>
2026-02-13 23:08:29 +0200

    updates

--- main.py
+++ main.py
@@ -32,6 +32,7 @@ app.add_middleware(
         "http://127.0.0.1:5173",
         "http://localhost:8000",
         "http://127.0.0.1:8000",
+        "http://accessories-backend-production.up.railway.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
